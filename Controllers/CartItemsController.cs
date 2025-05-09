bcc58615df38e583c535d523f51ac52c0f0352ec
bidhan-skybase <bidhan@skybase.com.np>
2025-05-09 17:24:18 +0545

    fix: Update add to cart functionality

--- Controllers/CartItemsController.cs
+++ Controllers/CartItemsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Ghayal_Bhaag.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +26,36 @@ namespace Ghayal_Bhaag.Controllers
             var applicationDbContext = _context.CartItem.Include(c => c.User).Where(item=>item.Status == Enums.OrderStatus.PENDING);
             return View(await applicationDbContext.ToListAsync());
         }
+        
+        public IActionResult CreateCartItem(int? id)
+        {
+            if (id != null)
+            {
+                Book? book = _context.Book.Find(id);
+                if (book != null)
+                {
+                    ViewData["UnitPrice"] = book.Price;
+                }
+                ViewData["BookId"] = id;
+            }
+            else
+            {
+                ViewData["BookId"] = new SelectList(_context.Book, "BookId", "BookId");
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+        
+        // IMPORTANT: Keep this method as it might still be referenced from other places
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCartItem([Bind("CartItemId,UserId,BookId,Quantity,UnitPrice")] CartItem cartItem)
+        {
+            cartItem.Status = OrderStatus.PENDING;
+            _context.Add(cartItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(ListCartItems));
+        }
 
         // GET: CartItems/Details/5
         public async Task<IActionResult> GetCartDetails(int? id)
@@ -44,38 +76,46 @@ namespace Ghayal_Bhaag.Controllers
             return View(cartItem);
         }
 
-        // GET: CartItems/Create
-        public IActionResult CreateCartItem(int? id)
-        {
-
-            if ( id != null)
-            {
-                Book? book = _context.Book.Find(id);
-                if(book != null)
-                {
-                    ViewData["UnitPrice"] = book.Price;
-                }
-                ViewData["BookId"] = id;
-            }
-            else
-            {
-                ViewData["BookId"] = new SelectList(_context.Book, "BookId", "BookId");
-            }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
-            return View();
-        }
-
-        // POST: CartItems/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateCartItem([Bind("CartItemId,UserId,BookId,Quantity,UnitPrice")] CartItem cartItem)
+        public async Task<IActionResult> AddToCart(int id, int quantity)
         {
-            cartItem.Status = Enums.OrderStatus.PENDING;
-                _context.Add(cartItem);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(ListCartItems));
+            // Find the book
+            Book? book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            
+            // Validate quantity against stock
+            if (quantity <= 0)
+            {
+                quantity = 1;
+            }
+            else if (quantity > book.Stock)
+            {
+                quantity = book.Stock;
+            }
+            
+            // Get current user ID - replace this with your actual user ID retrieval method
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // If using Identity
+            
+            // Create cart item
+            CartItem cartItem = new CartItem
+            {
+                UserId = userId,
+                BookId = id,
+                Quantity = quantity,
+                UnitPrice = book.Price,
+                Status = OrderStatus.PENDING
+            };
+            
+            // Add to database
+            _context.Add(cartItem);
+            await _context.SaveChangesAsync();
+            
+            // Redirect to cart
+            return RedirectToAction(nameof(ListCartItems));
         }
 
         // GET: CartItems/Edit/5
--- obj/Debug/net9.0/LearnDeck.AssemblyInfo.cs
+++ obj/Debug/net9.0/LearnDeck.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnDeck")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f0b4ac5331692cea1cdcfef3dfc7fe55dbd984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86156bf5ef3cdb611dd0c4219fd50a0172f7c1d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnDeck")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnDeck")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
